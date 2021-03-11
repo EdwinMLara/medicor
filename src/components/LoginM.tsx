f3518d8b75e376f5d55c02306f027b6c96d55572
EdwinMLara <emlara35@gmail.com>
2021-03-11 15:04:15 -0600

    Agregando react formick

--- src/components/LoginM.tsx
+++ src/components/LoginM.tsx
@@ -6,6 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
@@ -23,25 +26,68 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-function LoginM() {
+interface LoginValues {
+    username : string,
+    password : string
+}
+
+const initialValues : LoginValues = {
+    username : '',
+    password : ''
+}
+
+const validationSchema = yup.object({
+    username: yup
+        .string()
+        .required("El usuario es requerido"),
+    password: yup
+        .string()
+        .min(8, 'La contraseña necesita al menos 8 caracteres')
+        .required("La constraseña es requerida")
+  });
+
+
+function LoginM() : JSX.Element {  
+
     const classes = useStyles();
+    const formik = useFormik({
+        initialValues,
+        validationSchema: validationSchema,
+            onSubmit: (values) => {
+                console.log(values);
+            }
+    });
 
     return (
         <Card className={classes.root}>
             <CardHeader className={classes.header}
                 title="Iniciar Sessión"
-                //subheader="Uriangato Gto" 
-                />
+            />
 
             <CardContent>
-                <form noValidate autoComplete="off">
-                    <TextField id="username" label="Nombre de usuario" fullWidth/>  
-                    <TextField id="password" label="Contraseña" fullWidth/>  
+                <form onSubmit={formik.handleSubmit} autoComplete="off">
+                    <TextField fullWidth
+                        id="username" 
+                        label="Nombre de usuario" 
+                        name="username" 
+                        value = {formik.values.username}
+                        onChange = {formik.handleChange}
+                        error={formik.touched.username && Boolean(formik.errors.username)}
+                        helperText={formik.touched.username && formik.errors.username}
+                        />  
+                    <TextField fullWidth
+                        id="password" 
+                        label="Contraseña" 
+                        name="password" 
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                        />
+                    <Button type="submit" variant="contained" color="primary" fullWidth>Primary</Button>  
                 </form>
             </CardContent>
-            <Button variant="contained" color="primary" fullWidth>
-                Primary
-            </Button>
         </Card>
     )
 }
