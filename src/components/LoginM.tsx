f3d44021a0756b4e1562acc9d4f908cf6a8d0153
EdwinMLara <emlara35@gmail.com>
2021-03-12 12:44:11 -0600

    analizar render

--- src/components/LoginM.tsx
+++ src/components/LoginM.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useContext } from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -6,8 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-import { useFormik } from 'formik';
+import { FormikConsumer, useFormik } from 'formik';
 import * as yup from 'yup';
+import { IsLoggedContext } from '../App';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -48,6 +49,8 @@ const validationSchema = yup.object({
 
 
 function LoginM() : JSX.Element {  
+    console.log("iniciando");
+    
 
     const classes = useStyles();
     const formik = useFormik({
