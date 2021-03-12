f31e054a0e32062678051b08099c84a3bc803966
EdwinMLara <emlara35@gmail.com>
2021-03-12 13:56:55 -0600

    Agregar contexto sin actualizar estado

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import LoginM from './components/LoginM';
 const arrayAdmin : Array<string> = ['Inicio','Consultas','Pacientes','Titulares','Medicos','Usuarios'];
 // const arrayMedico : Array<string> = ['Inicio','Consultas','Pacientes'];
 // const arrayFarmacia : Array<string> = ['Recetas','Medicamentos','Reportes'];
+export const IsLoggedContext = React.createContext<boolean>(false);
 
 function App() {
   return (
     <div className="App">
-      <Drawer menu={arrayAdmin} islogged={false}/>
-      <LoginM/>
+      <IsLoggedContext.Provider value={false}>
+        <Drawer menu={arrayAdmin} islogged={false}/>
+        <LoginM/>
+      </IsLoggedContext.Provider>
     </div>
   );
 }
