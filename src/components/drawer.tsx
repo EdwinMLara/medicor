b5a4fde7e811573b5fc8d256510c7c5f19ffc90e
EdwinMLara <emlara35@gmail.com>
2021-03-14 00:48:10 -0600

    Ajustando el drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -15,17 +15,31 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
+import HomeIcon from '@material-ui/icons/Home';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import AirlineSeatFlatIcon from '@material-ui/icons/AirlineSeatFlat';
 import { Fragment } from 'react';
 
 import { IsLoggedContext } from '../App';
+import Button from '@material-ui/core/Button';
+
+import { withRouter } from 'react-router-dom';
+
+
+import { Route, Switch } from "react-router-dom";
+import Consultas from './Consultas';
+import Inicio from './Inicio';
+import Pacientes from './Pacientes';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
+    },
+    grow:{
+        flexGrow: 1
     },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
@@ -86,16 +100,36 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 interface DataDrawer{
-    menu : Array<string>
+    text : string,
+    icon : any,
+    onClick : (props : any) => any 
 }
 
-function DrawerM(props:DataDrawer) {
+function DrawerM(props : any) {
+    const { history } = props;
 
     const classes = useStyles();
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
     const {islogged} = useContext(IsLoggedContext);
+
+    const arrayAdmin : Array<DataDrawer> = [
+        {
+            text : 'Inicio',
+            icon :  <HomeIcon/>,
+            onClick : () => history.push("/")
+        },
+        {
+            text : 'Consultas',
+            icon :  <LocalHospitalIcon/>,
+            onClick : () => history.push("/consultas")
+        },
+        {
+            text : 'Pacientes',
+            icon :  <AirlineSeatFlatIcon/>,
+            onClick : () => history.push("/pacientes")
+        }];
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -131,6 +165,11 @@ function DrawerM(props:DataDrawer) {
                     <Typography variant="h6" noWrap>
                         Medico
                     </Typography>
+                    {islogged ? <React.Fragment>
+                                    <div className={classes.grow}></div>
+                                    <Button color="inherit">Log out</Button>
+                                </React.Fragment> : null }
+                    
                 </Toolbar>
             </AppBar>
             {islogged ?
@@ -155,43 +194,30 @@ function DrawerM(props:DataDrawer) {
                 </div>
                 <Divider />
                 <List>
-                    {(props.menu).map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
+                    {
+                        arrayAdmin.map((item : DataDrawer, index : number) => {       
+                        const {text,icon,onClick} = item
+                        return(
+                            <ListItem button key={text} onClick={onClick}>
+                                {icon && <ListItemIcon>{icon}</ListItemIcon>}
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        )})
+                    }
                 </List>
             </Drawer> 
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+                
+                <Switch>
+                    <Route exact path="/" render={props => <Inicio/>} />
+                    <Route exact path="/consultas" render={props => <Consultas/>} />
+                    <Route exact path="/pacientes" render={props => <Pacientes />} />
+                </Switch>
             </main> 
             </Fragment> : null}
         </div> 
     )
 }
 
-export default DrawerM;
+export default withRouter(DrawerM);
