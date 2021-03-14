6dc666dc606cae5268b364825ff986d40dd83385
EdwinMLara <emlara35@gmail.com>
2021-03-14 00:49:00 -0600

    Eliminando el error de router

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { boolean } from 'yup/lib/locale';
 import Drawer from './components/drawer';
 import LoginM from './components/LoginM';
 
-import { Route, Switch } from "react-router-dom";
-import Consultas from './components/Consultas';
-
 export interface LoggedContex  {
   islogged : boolean,
   setislogged : ((prevState: any) => any)
@@ -25,11 +22,6 @@ function App() {
       <IsLoggedContext.Provider value={{islogged,setislogged}}>
         <Drawer/>
         {!islogged ? <LoginM/> : null}
-        <Switch>
-        <Route exact from string ="/" render={props => <Consultas {...props} />} />
-        <Route exact path="/contact" render={props => <Contact {...props} />} />
-        <Route exact path="/about" render={props => <About {...props} />} />
-      </Switch>
       </IsLoggedContext.Provider>
     </div>
   );
