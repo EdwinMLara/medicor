95c8eb6a2d26bbe34ddbc18a6f66a034fd57117a
EdwinMLara <Valiant.ecatepec1>
2021-03-29 15:36:49 -0600

    Reconfigurando el formick

--- src/components/consultas/NewConsulta.tsx
+++ src/components/consultas/NewConsulta.tsx
@@ -1,13 +1,11 @@
-import {useFormik} from 'formik'
-import { Button, TextField,Grid, MenuItem, makeStyles } from "@material-ui/core"
-import React, { useState } from 'react';
+import {Formik,Form,Field, FieldArray} from 'formik'
+import { Button, TextField,Grid, makeStyles } from "@material-ui/core"
+import React from 'react';
 import * as yup from 'yup';
 
 import axios from "axios";
 import Receta from '../pdf/Receta';
 import ReactDOM from 'react-dom';
-
-import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
     paddingButton: {
@@ -18,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
 interface MedicamentosValues{
     cantidad : number,
     nombre: string,
-    prescirpcion: string
+    prescripcion: string
 }
 
 interface ConsultaValues{
@@ -32,7 +30,7 @@ interface ConsultaMedicamentos{
     receta:MedicamentosValues[]
 }
 
-const validationSchemaConsulta = yup.object({
+const validationSchemaConsultaMedicamentos = yup.object({
     paciente: yup.object({
         sintomas: yup.string().required("Agregar sintomas del paciente"),
         diagnostico: yup.string().required("Agregar diagnostico")
@@ -41,20 +39,24 @@ const validationSchemaConsulta = yup.object({
 
 function NewConsulta(props : any) {
     const {paciente} = props.location;
-    console.log(paciente);
 
-    const classes = useStyles();
+    const initialValuesReceta : MedicamentosValues = {
+        cantidad:0,
+        nombre:'',
+        prescripcion:''
+    }
 
-    const initialValuesConsulta : ConsultaMedicamentos = {
-        paciente :  {
-            idPaciente:paciente._id,
-            sintomas:'',
-            diagnostico:''
-        },
-        receta:[]
+    const initialValuesConsulta : ConsultaValues = {   
+        idPaciente:paciente._id,
+        sintomas:'',
+        diagnostico:''
+    }
+
+    const initialValuesConsultaMedicamentos : ConsultaMedicamentos = {
+        paciente : initialValuesConsulta ,
+        receta:[initialValuesReceta]
     }   
 
-    console.log(initialValuesConsulta);
 
     const sendPostRequest = async (body : ConsultaValues) =>{
        let response = await axios.post('http://localhost:5000/consultas/insert',body)
@@ -67,97 +69,74 @@ function NewConsulta(props : any) {
         console.log(response);
     }
 
-    const formik = useFormik({
-        initialValues: initialValuesConsulta,
-        validationSchema:validationSchemaConsulta,
-        onSubmit:(values) =>{
-            console.log(values);
-            /*let stringValues = values.paciente;
-            for(let key in stringValues){
-                stringValues[key] = stringValues[key].trim();
-            }
-            values.paciente = stringValues;
-            console.log(values);
-            let elemet = document.createElement('div');
-            ReactDOM.render(<Receta {...values}/>, elemet);
-            window.open("", "Receta", "width=520,height=650")?.document.body.appendChild(elemet);
-            sendPostRequest(values);*/
-        }
-    });
-
-    const [cantidadM,SetCantidadM] = useState(1);
-
-    const handleChangeCantidadM = (e :  React.ChangeEvent<HTMLInputElement>) =>{
-        SetCantidadM(parseInt(e.target.value));
-    }
     return (
         <React.Fragment>
             <h1>Nueva Consulta</h1>
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={8}>
-                    <form onSubmit={formik.handleSubmit} autoComplete="off">
-                            <TextField disabled fullWidth
-                                id="nombre" 
-                                label="Nombre del Paciente" 
-                                name="nombre" 
-                                value={paciente.nombre}
-                            />
-                            <TextField fullWidth
-                                id="sintomas" 
-                                label="Sintomas del Paciente" 
-                                name="sintomas" 
-                                value={formik.values.paciente?.sintomas}
-                                onChange={formik.handleChange}
-                                error={formik.touched.paciente?.sintomas && Boolean(formik.errors.paciente?.sintomas)}
-                                helperText={formik.touched.paciente?.sintomas && formik.errors.paciente?.sintomas}
-                            />  
-                            <TextField fullWidth
-                                id="diagnostico" 
-                                label="Diagnostico" 
-                                name="diagnostico" 
-                                value={formik.values.paciente?.diagnostico}
-                                onChange={formik.handleChange}
-                                error={formik.touched.paciente?.diagnostico && Boolean(formik.errors.paciente?.diagnostico)}
-                                helperText={formik.touched.paciente?.diagnostico && formik.errors.paciente?.diagnostico}
+            <Grid item xs={12} sm={8}>
+                    <Formik
+                        initialValues={initialValuesConsultaMedicamentos}
+                        validationSchema={validationSchemaConsultaMedicamentos}
+                        onSubmit={(values : any) => {
+                            console.log('Form data', values)
+                         }
+                        }
+                    >
+
+                    {formik =>{
+                        console.log(formik);
+                        return (
+                            <Form onSubmit={formik.handleSubmit}>
+                                <TextField disabled fullWidth
+                                    id="nombre" 
+                                    label="Nombre del Paciente" 
+                                    name="nombre" 
+                                    value={paciente.nombre}
+                                />
+                                <TextField fullWidth
+                                    id="sintomas" 
+                                    label="Sintomas del Paciente" 
+                                    name="paciente.sintomas"
+                                    value={formik.values.paciente.sintomas}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.paciente?.sintomas && Boolean(formik.errors.paciente?.sintomas)}
+                                    helperText={formik.touched.paciente?.sintomas && formik.errors.paciente?.sintomas}/>  
+                                <TextField fullWidth
+                                    id="diagnostico" 
+                                    label="Diagnostico" 
+                                    name="paciente.diagnostico"
+                                    value={formik.values.paciente.diagnostico}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.paciente?.diagnostico && Boolean(formik.errors.paciente?.diagnostico)}
+                                    helperText={formik.touched.paciente?.diagnostico && formik.errors.paciente?.diagnostico}
                                 />
 
-                            <Grid container>
-                                <Grid item xs={12} sm={7}>
-                                    <TextField fullWidth
-                                        id="medicamento" 
-                                        label="medicamento" 
-                                        name="medicamento" 
-                                    />
-                                </Grid>
-                                <Grid item xs={8} sm={3}>
-                                    <TextField
-                                        id="cantidad"
-                                        select
-                                        label="Cantidad"
-                                        name="cantidad"
-                                        value={cantidadM}
-                                        onChange={handleChangeCantidadM}
-                                        helperText="Selecciona la cantidad de medicamentos"
-                                        >
-                                        {[1,2,3,4,5].map((option) => (
-                                            <MenuItem key={option} value={option}>
-                                                {option}
-                                            </MenuItem>
-                                        ))}
-                                    </TextField>
-                                </Grid>
-                                <Grid item xs={4} sm={2} className={classes.paddingButton}>
-                                    <Button type="button" variant="contained" size="small">
-                                            <AddIcon/>  
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                            <Button type="submit" 
-                                variant="contained" 
-                                color="primary" fullWidth>
-                                    imprimir
-                            </Button>  
-                    </form>
+                                <FieldArray
+                                    name="receta"
+                                    render={
+                                        (arrayHelpers:any) =>{
+                                            const {push,remove,form} = arrayHelpers;
+                                            const {values} = form;
+                                            const {receta} = values;
+                                            console.log(arrayHelpers);
+                                            return ({
+                                                receta.map((medicamento : MedicamentosValues,index : number) =>{
+                                                    return(<div key={index}>{medicamento.nombre}</div>)
+                                                })
+                                            })
+                                        }}
+                                />
+
+                                <Button type="submit" 
+                                    variant="contained" 
+                                    color="primary" fullWidth>
+                                        imprimir
+                                </Button>  
+                            </Form>
+                        )
+                    }}
+                    
+                </Formik>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <h2>Receta</h2>
