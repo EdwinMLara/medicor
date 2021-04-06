1aafe5afe539d49a71ded53ea0a7dc98d0811b37
EdwinMLara <Valiant.ecatepec1>
2021-04-06 13:43:33 -0500

    elimando basura

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,6 @@ import LoginM from './components/LoginM';
 import {useSelector} from 'react-redux'
 import {RootReducerType} from './components/redux/rootReducer';
 
-
-/*export interface LoggedContex  {
-  islogged : boolean,
-  setislogged : ((prevState: any) => any)
-}
-
-export const loggedDefaultContext: LoggedContex = {
-  islogged : false,
-  setislogged : ((prevState: boolean) => boolean)
-}
-export const IsLoggedContext = React.createContext<LoggedContex>(loggedDefaultContext);*/
-
 function App() {
   const islogged = useSelector((state : RootReducerType) => state.loggin.statusLoggin);
 
