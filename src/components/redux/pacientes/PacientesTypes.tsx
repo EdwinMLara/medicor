14eb6fe6dc06df4cb1672beb1e84ba1c03191ab1
EdwinMLara <emlara35@gmail.com>
2022-04-20 23:35:02 -0500

    se agrego interfaz de paciente o se movio

--- src/components/redux/pacientes/PacientesTypes.tsx
+++ src/components/redux/pacientes/PacientesTypes.tsx
@@ -2,9 +2,10 @@ export const FETCH_PACIENTS_REQUETS = 'FETCH_PACIENTS_REQUETS';
 export const FETCH_PACIENTS_SUCCESS = 'FETCH_PACIENTS_SUCCESS';
 export const FETCH_PACIENTS_FAILURE = 'FETCH_PACIENTS_FAILURE';
 export const CURRENT_PACIENT_STATUS = 'CURRENT_PACIENT_STATUS';
+export const FETCH_COUNT_PACIENTS = 'FETCH_COUNT_PACIENTS';
 
 export type ActionTypes =  'FETCH_PACIENTS_REQUETS' | 'FETCH_PACIENTS_SUCCESS'
- | 'FETCH_PACIENTS_FAILURE' | 'CURRENT_PACIENT_STATUS';
+ | 'FETCH_PACIENTS_FAILURE' | 'CURRENT_PACIENT_STATUS' | 'FETCH_COUNT_PACIENTS';
 
 export type Talla = 'ch' | 'md' | 'gr';
 
@@ -16,4 +17,12 @@ export interface PacienteFormValues{
     enfermedadesCronicas: string,
     alergias: string,
     imageb64:String
+}
+
+export interface StateValues {
+    count:number,
+    loading: boolean,
+    pacients: PacienteFormValues[],
+    error : string,
+    currentPacient : PacienteFormValues
 }
